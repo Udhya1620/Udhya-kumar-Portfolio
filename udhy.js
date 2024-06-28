9fe32af19cdf90aba40ed8b1022c67c61094714a
Udhaya <udhyakumar162000@gmail.com>
2024-06-28 20:10:41 +0530

    Added responsiveness
    Corrected mistakes in the code and added responsiveness and yet to add code for navbar to be visible for the mobile and tablet view

--- udhy.js
+++ udhy.js
@@ -7,13 +7,14 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-
 /*==================== scroll sections active link ====================*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+
 
 window.onscroll = () => {
-    sections.forEach(sec => {
+
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+        sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
@@ -22,19 +23,23 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySe1ector('header nav a[href*=' + id + ']').c1assList.add('active');
-
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        };
+        }
     });
 
     /*==================== sticky navbar ====================*/
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 };
+
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-   menuIcon.classList.toggle('bx-x');
-   navbar.classList.toggle('active');
+navLinks.forEach(link => {
+    link.onclick = () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    };
+});
 
 /*==================== scroll reveal ====================*/
 ScrollReveal({
@@ -44,9 +49,17 @@ ScrollReveal({
     delay: 200
 });
 
-    ScrollRevea1().reveal('.home-content, .heading', { origin: 'top'});
-    ScrollRevea1().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
-    ScrollRevea1().reveal('.home-content h1, .about-img', { origin: 'left'});
-    ScrollRevea1().reveal('.home-content p, .about-content', { origin: 'right'});
-   
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
 /*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
